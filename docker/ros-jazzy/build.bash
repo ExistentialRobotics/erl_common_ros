96770891de24efd46c29457c13e593878be4790d
daizhirui <daizhirui@hotmail.com>
2026-01-23 17:13:21 -0800

    fix ros-jazzy dockerfile

--- docker/ros-jazzy/build.bash
+++ docker/ros-jazzy/build.bash
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 
+BASE_IMAGE="${BASE_IMAGE:-"erl/common:24.04"}"
 ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 docker build --rm -t erl/ros-jazzy:cpu-common . \
-    --build-arg BASE_IMAGE=erl/common:24.04 \
-    --build-arg ROS_APT_SOURCE_VERSION=$ROS_APT_SOURCE_VERSION $@
+    --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+    --build-arg ROS_APT_SOURCE_VERSION="${ROS_APT_SOURCE_VERSION}" $@
